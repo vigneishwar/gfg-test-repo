0c7e54ee63968a298f155a02033789728d532b82
root <root@WayneCorp>
2023-05-08 01:58:37 +1000

    vigneishwar commit in new class.java file for github actions

--- src/main/java/pkg1/NewClass.java
+++ src/main/java/pkg1/NewClass.java
@@ -5,6 +5,7 @@ public class NewClass {
 	public static void main(String[] args) {
 
 		System.out.println("Hello Test from Vigneishwar");
+		System.out.println("Hello Test from Vigneishwar for github actions");
 
 	}
 
